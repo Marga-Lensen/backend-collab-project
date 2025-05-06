1a310bc9fceac4a564ff8d57502319cf1626e68a
Marga-Lensen <marga.lensen277@gmail.com>
2025-05-06 17:55:22 +0200

    index all in one

--- frontend/config.js
+++ frontend/config.js
@@ -4,4 +4,4 @@
 // Beispiel: Wenn im Backend .env steht PORT=3001, dann:
 // const API_BASE_URL = "http://localhost:3001";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "http://localhost:5000";
